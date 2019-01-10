dad681be727cd6873bfe9386c9e8f805d07527e2
Umayr <umayr23@gmail.com>
2019-01-10 17:36:24 +0000

    Search functionality added to App

--- src/SearchDB.js
+++ src/SearchDB.js
@@ -5,24 +5,42 @@ class SearchDB extends Component{
     constructor(){
         super();
         this.state = {
-            movies: "hi"
+            movies: "hi",
+            searchQuery: ""
         }
         this.update = this.update.bind(this)
     }
 
+    handleInput = (event) =>{
+        this.setState({searchQuery: document.getElementById("filmSearch").value})
+    }
+
+    handleSubmit = (event) => {
+    // console.log('A name was submitted: ' + this.state.stateText);
+    event.preventDefault();
+    }
     
     update(){
         axios({
             method: 'get',
-            url:'http://www.omdbapi.com/?i=tt3896198&apikey=cba3c9de',
+            url:'http://www.omdbapi.com/?t='+this.state.searchQuery+'&apikey=cba3c9de',
             responseType:'json'
-        }).then(response => this.setState({movies:response.data}));
-        console.log(this.state.movies);  
+        }).then(response =>{ 
+            this.setState({movies:response.data});
+        console.log(this.state.movies)
+        });  
     }
     
     render(){
         return(
-            <button onClick={this.update}>Click Me</button>
+            <form >
+                <label>Search Film by Title:</label>
+                <br></br>
+                <input id="filmSearch" type="text" onChange={(this.handleInput)}></input>
+                <input type="button" value="Submit" onClick={this.update}></input>
+            </form>
+            // <input type="text"></input>
+            // <button onClick={this.update}>Click Me</button>
             // console.log(this.update())
         );
     }
